8117f94ec3ed7bac6efb8667cb314c410f6701ce
Philipp Stephani <phst@google.com>
2015-01-16 14:01:57 +0100

    Add DISPLAY environment variable if not set so that notifications can be displayed from a cron job

--- notify_linux.go
+++ notify_linux.go
@@ -1,8 +1,22 @@
 package main
 
-import "os/exec"
+import (
+	"os"
+	"os/exec"
+	"strings"
+)
 
 func (p problem) notify() error {
 	cmd := exec.Command("notify-send", "--", "gitnag", p.String())
+	cmd.Env = addDisplayEnv(os.Environ())
 	return cmd.Run()
+}
+
+func addDisplayEnv(env []string) []string {
+	for _, s := range env {
+		if strings.HasPrefix(s, "DISPLAY=") {
+			return env
+		}
+	}
+	return append(env, "DISPLAY=:0")
 }
