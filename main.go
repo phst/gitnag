d6c7e2ed5156e0d9c11e31b0d9d763601aedf772
Philipp Stephani <phst@google.com>
2019-01-09 04:13:46 +0100

    don’t print a stacktrace in main

--- main.go
+++ main.go
@@ -11,16 +11,16 @@ var configFile = flag.String("config", "", "location of the configuration file")
 func main() {
 	flag.Parse()
 	if flag.NArg() > 0 {
-		glog.Fatal("this program doesn't accept positional arguments")
+		glog.Exit("this program doesn't accept positional arguments")
 	}
 	if *configFile == "" {
-		glog.Fatal("-config flag must be provided")
+		glog.Exit("-config flag must be provided")
 	}
 	cfg, err := loadConfig(*configFile)
 	if err != nil {
-		glog.Fatalf("could not load configuration: %v", err)
+		glog.Exitf("could not load configuration: %v", err)
 	}
 	if err := run(*cfg); err != nil {
-		glog.Fatal(err)
+		glog.Exit(err)
 	}
 }
